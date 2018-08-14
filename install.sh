aa3e5449010cc5dc90bfba8a6d6bcbb895c4a477
Thomas Tardy <tardy@mptechnology.ch>
2018-08-14 22:24:03 +0200

    updaate dock config

--- install.sh
+++ install.sh
@@ -775,7 +775,9 @@ running "Remove the animation when hiding/showing the Dock"
 defaults write com.apple.dock autohide-time-modifier -float 0;ok
 
 running "Automatically hide and show the Dock"
-defaults write com.apple.dock autohide -bool true;ok
+# defaults write com.apple.dock autohide -bool true;ok
+running "Don't automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool false;ok
 
 running "Make Dock icons of hidden applications translucent"
 defaults write com.apple.dock showhidden -bool true;ok
