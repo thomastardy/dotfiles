1ba8af6222285d6454b0ad58ba9dd46f5c3c4f8a
Thomas Tardy <tardy@mptechnology.ch>
2018-08-14 22:58:31 +0200

    update todos

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   brew: [
     // http://conqueringthecommandline.com/book/ack_ag
-    // TODO check if required 'ack',
-    // TODO check if required 'ag',
+    // TODO tardy check if required 'ack',
+    // TODO tardy check if required 'ag',
     // Install GNU core utilities (those that come with macOS are outdated)
     // Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
     'coreutils',
@@ -10,33 +10,33 @@ module.exports = {
     // Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
     'findutils',
     // 'fortune',
-    // TODO check if required 'readline', // ensure gawk gets good readline
-    // TODO check if required 'gawk',
+    // todo-txt check if required 'readline', // ensure gawk gets good readline
+    // TODO tardy check if required 'gawk',
     // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    // TODO check if required 'gifsicle',
-    // TODO check if required 'gnupg',
+    // TODO tardy check if required 'gifsicle',
+    // TODO tardy check if required 'gnupg',
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
-    // TODO check if required 'gnu-sed --with-default-names',
+    // TODO tardy check if required 'gnu-sed --with-default-names',
     // better, more recent grep
     'homebrew/dupes/grep',
     // https://github.com/jkbrzt/httpie
-    // TODO check if required 'httpie',
+    // TODO tardy check if required 'httpie',
     // jq is a sort of JSON grep
-    // TODO check if required 'jq',
+    // TODO tardy check if required 'jq',
     // Mac App Store CLI: https://github.com/mas-cli/mas
-    // TODO check if required 'mas',
+    // TODO tardy check if required 'mas',
     // Install some other useful utilities like `sponge`
-    // TODO check if required 'moreutils',
+    // TODO tardy check if required 'moreutils',
     'nmap',
-    // TODO check if required 'openconnect',
-    // TODO check if required 'reattach-to-user-namespace',
+    // TODO tardy check if required 'openconnect',
+    // TODO tardy check if required 'reattach-to-user-namespace',
     // better/more recent version of screen
-    // TODO check if required 'homebrew/dupes/screen',
-    // TODO check if required 'tmux',
-    // TODO check if required 'todo-txt',
-    // TODO check if required 'tree',
-    // TODO check if required 'ttyrec',
+    // TODO tardy check if required 'homebrew/dupes/screen',
+    // TODO tardy check if required 'tmux',
+    // TODO tardy check if required 'todo-txt',
+    // TODO tardy check if required 'tree',
+    // TODO tardy check if required 'ttyrec',
     // better, more recent vim
     'vim --with-override-system-vi',
     // TODO check if required 'watch',
