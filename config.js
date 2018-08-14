47a5dab3a6bcb8baf57d10c890c91e6a07dfa7bd
Thomas Tardy <tardy@mptechnology.ch>
2018-08-14 21:22:45 +0200

    copy configs

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   brew: [
     // http://conqueringthecommandline.com/book/ack_ag
-    'ack',
-    'ag',
+    // TODO check if required 'ack',
+    // TODO check if required 'ag',
     // Install GNU core utilities (those that come with macOS are outdated)
     // Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
     'coreutils',
@@ -10,38 +10,43 @@ module.exports = {
     // Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
     'findutils',
     // 'fortune',
-    'readline', // ensure gawk gets good readline
-    'gawk',
+    // TODO check if required 'readline', // ensure gawk gets good readline
+    // TODO check if required 'gawk',
     // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
-    'gnupg',
+    // TODO check if required 'gifsicle',
+    // TODO check if required 'gnupg',
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
-    'gnu-sed --with-default-names',
+    // TODO check if required 'gnu-sed --with-default-names',
     // better, more recent grep
     'homebrew/dupes/grep',
     // https://github.com/jkbrzt/httpie
-    'httpie',
+    // TODO check if required 'httpie',
     // jq is a sort of JSON grep
-    'jq',
+    // TODO check if required 'jq',
     // Mac App Store CLI: https://github.com/mas-cli/mas
-    'mas',
+    // TODO check if required 'mas',
     // Install some other useful utilities like `sponge`
-    'moreutils',
+    // TODO check if required 'moreutils',
     'nmap',
-    'openconnect',
-    'reattach-to-user-namespace',
+    // TODO check if required 'openconnect',
+    // TODO check if required 'reattach-to-user-namespace',
     // better/more recent version of screen
-    'homebrew/dupes/screen',
-    'tmux',
-    'todo-txt',
-    'tree',
-    'ttyrec',
+    // TODO check if required 'homebrew/dupes/screen',
+    // TODO check if required 'tmux',
+    // TODO check if required 'todo-txt',
+    // TODO check if required 'tree',
+    // TODO check if required 'ttyrec',
     // better, more recent vim
     'vim --with-override-system-vi',
-    'watch',
+    // TODO check if required 'watch',
     // Install wget with IRI support
-    'wget --enable-iri'
+    'wget --enable-iri',
+    'maven',
+    'netcat',
+    'ssh-copy-id',
+    'sshpass',
+    'telnet'
   ],
   cask: [
     //'adium',
@@ -53,37 +58,52 @@ module.exports = {
     'docker', // docker for mac
     //'dropbox',
     //'evernote',
-    'flux',
-    'gpg-suite',
+    // 'flux',
+    // 'gpg-suite',
     //'ireadfast',
     'iterm2',
-    'little-snitch',
-    'macbreakz',
-    'micro-snitch',
-    'signal',
+    // 'little-snitch',
+    // 'macbreakz',
+    // 'micro-snitch',
+    // 'signal',
     //'macvim',
-    'sizeup',
+    // 'sizeup',
     //'sketchup',
     'slack',
     'the-unarchiver',
     //'torbrowser',
     //'transmission',
-    'visual-studio-code',
+    // 'visual-studio-code',
     //'vlc',
-    'xquartz'
+    // 'xquartz',
+    'alfred',
+    'appcleaner',
+    'fantastical',
+    'keepassxc',
+    'keepingyouawake',
+    'qlcolorcode',
+    'qlmarkdown',
+    'qlprettypatch',
+    'qlstephen',
+    'quicklook-json',
+    'sourcetree',
+    'sublime-text',
+    'viscosity',
+    'vnc-viewer',
+    'whatsapp'
   ],
   gem: [
   ],
   npm: [
-    'antic',
-    'buzzphrase',
-    'eslint',
-    'instant-markdown-d',
+    // 'antic',
+    // 'buzzphrase',
+    // 'eslint',
+    // 'instant-markdown-d',
     // 'generator-dockerize',
     // 'gulp',
     'npm-check-updates',
-    'prettyjson',
-    'trash',
+    // 'prettyjson',
+    // 'trash',
     'vtop'
     // ,'yo'
   ]
